411e9a67bc7d3b169b1345bd2dae30d132d6241b
Arthur <grigorevich1993artur@gmail.com>
2021-01-09 01:33:48 +0400

    firebase configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz2QqITlDORUlJOMKHjgFLGpe3ZRIFNT8",
+  authDomain: "fir-test-project-6d951.firebaseapp.com",
+  databaseURL: "https://fir-test-project-6d951-default-rtdb.firebaseio.com",
+  projectId: "fir-test-project-6d951",
+  storageBucket: "fir-test-project-6d951.appspot.com",
+  messagingSenderId: "519500804019",
+  appId: "1:519500804019:web:33710736a52736436d25c6"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from 'firebase';
 
 export const Home = function () {
-    return (
-        <div>Home</div>
-    )
+
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  useEffect( () => {
+    const db = firebase.database();
+    console.log(db);
+  }, []);
+
+  const handleChange = ({ target: { value, name } }) => {
+    setForm({...form, [name]: value});
+  };
+
+  const createAccount = () => {
+    firebase.auth().signInWithEmailAndPassword(form.email, form.password)
+      .catch(error => console.log(error))
+  };
+
+  return (
+    <div>
+      <input type="text" name="email" onChange={handleChange}/>
+      <input type="password" name="password" onChange={handleChange}/>
+      <input type="submit" onClick={createAccount}/>
+    </div>
+  )
 };
